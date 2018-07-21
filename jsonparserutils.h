6b49a4633a3b72b37d52b85fab1f44999ab41a99
Daniel Palmer <daniel@0x0f.com>
2018-07-21 21:48:11 +0900

    Initial commit

---
+++ jsonparserutils.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define JSON_NODE_GET_OBJECT(n) (JSON_NODE_HOLDS_OBJECT(n) ? json_node_get_object(n) : NULL)
+#define JSON_OBJECT_GET_MEMBER_STRING(o,m) (json_object_has_member(o, m) ? json_object_get_string_member(o, m) : NULL)
+#define JSON_OBJECT_GET_MEMBER_ARRAY(o, m) (json_object_has_member(o, m) ? json_object_get_array_member(o, m) : NULL)
