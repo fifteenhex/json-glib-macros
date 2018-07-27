dd1ff557da31d3af88e6e727c8f0d818098002a2
Daniel Palmer <daniel@0x0f.com>
2018-07-28 00:53:48 +0900

    Get unsigned int

--- jsonparserutils.h
+++ jsonparserutils.h
@@ -4,3 +4,4 @@
 #define JSON_OBJECT_GET_MEMBER_STRING(o,m) (json_object_has_member(o, m) ? json_object_get_string_member(o, m) : NULL)
 #define JSON_OBJECT_GET_MEMBER_ARRAY(o, m) (json_object_has_member(o, m) ? json_object_get_array_member(o, m) : NULL)
 #define JSON_OBJECT_GET_MEMBER_OBJECT(o, m) (json_object_has_member(o, m) ? json_object_get_object_member(o, m) : NULL)
+#define JSON_OBJECT_GET_MEMBER_INT(o, m) (json_object_has_member(o, m) ? json_object_get_int_member(o, m) : -1)
