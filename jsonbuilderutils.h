fec3aad78c25230aacb38604c8f73d38fea035fe
Daniel Palmer <daniel@0x0f.com>
2018-11-23 23:58:38 +0900

    Add add double

--- jsonbuilderutils.h
+++ jsonbuilderutils.h
@@ -3,6 +3,9 @@
 #define JSONBUILDER_ADD_INT(b, n, v) {json_builder_set_member_name(b, n);\
                                      json_builder_add_int_value(b, (int) v);}
 
+#define JSONBUILDER_ADD_DOUBLE(b, n, v) {json_builder_set_member_name(b, n);\
+                                     json_builder_add_double_value(b, (double) v);}
+
 #define JSONBUILDER_ADD_STRING(b, n, v) {json_builder_set_member_name(b, n);\
 	                                    json_builder_add_string_value(b, v);}
 
